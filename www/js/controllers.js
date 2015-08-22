0da3ed610e1beb9eebb64ae0f215db24feebec13
mannikma <mpmannikko@gmail.com>
2015-08-22 11:48:50 +0300

    Functionality to delete notifications.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -6,15 +6,21 @@ angular.module('starter.controllers', ['ionic', 'firebase'])
 .controller('DashCtrl', ['$scope', '$firebaseArray', function($scope, $firebaseArray){
 
   //$scope.activities = Activities;
-  var msgRef = new Firebase('https://brilliant-torch-2546.firebaseio.com/messages/');
+  var msgRef = new Firebase('https://samamuttaeri-5552.firebaseio.com/messages/');
 
 
   $scope.messages = $firebaseArray(msgRef);
 
   $scope.setAsRead = function(msg){
-    var readRef = new Firebase('https://brilliant-torch-2546.firebaseio.com/messages/' +
+    var readRef = new Firebase('https://samamuttaeri-5552.firebaseio.com/messages/' +
       msg.$id);
     readRef.child('isRead').set('true');
+  };
+
+  $scope.delNotification = function(msg){
+  	console.log("Meni tänne");
+  	var delRef = new Firebase('https://samamuttaeri-5552.firebaseio.com/messages/' + msg.$id);
+  	delRef.remove();
   };
 
 }]);
